ed3fd69e178945ef7d8a2a69b3bb5ceb25fe5b25
Alexandre Tison <alexandre.tison@decathlon.com>
2023-12-07 21:48:13 +0100

    Init Tinder Style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,59 @@
 'use client';
+import React, { useState } from 'react'
 import TinderCard from 'react-tinder-card'
 
-
+const db = [
+  {
+    name: 'Richard Hendricks',
+    url: './img/richard.jpg'
+  },
+  {
+    name: 'Erlich Bachman',
+    url: './img/erlich.jpg'
+  },
+  {
+    name: 'Monica Hall',
+    url: './img/monica.jpg'
+  },
+  {
+    name: 'Jared Dunn',
+    url: './img/jared.jpg'
+  },
+  {
+    name: 'Dinesh Chugtai',
+    url: './img/dinesh.jpg'
+  }
+]
 
 export default function Home() {
 
-const onSwipe = (direction: string) => {
-  console.log('You swiped: ' + direction)
-}
+  const characters = db
+  const [lastDirection, setLastDirection] = useState<string>()
 
-const onCardLeftScreen = (myIdentifier: string) => {
-  console.log(myIdentifier + ' left the screen')
-}
+  const swiped = (direction: string, nameToDelete: string) => {
+    console.log('removing: ' + nameToDelete)
+    setLastDirection(direction)
+  }
+
+  const outOfFrame = (name: string) => {
+    console.log(name + ' left the screen!')
+  }
 
 return (
-  <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['right', 'left']}>Hello, World!</TinderCard>
+  <div>
+  <link href='https://fonts.googleapis.com/css?family=Damion&display=swap' rel='stylesheet' />
+  <link href='https://fonts.googleapis.com/css?family=Alatsi&display=swap' rel='stylesheet' />
+  <h1>React Tinder Card</h1>
+  <div className='cardContainer'>
+    {characters.map((character) =>
+      <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+        <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
+          <h3>{character.name}</h3>
+        </div>
+      </TinderCard>
+    )}
+  </div>
+  {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />}
+</div>
 )
 }
