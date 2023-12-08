7d1831d33da34df587504353d286408916f5b81c
Theo Herbin <theo.herbin59710@outlook.fr>
2023-12-08 02:55:11 +0100

    Buttons (TO FIX)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 import React, { useState } from 'react'
 import TinderCard from 'react-tinder-card'
+import Button from './components/button/Button';
+
 
 const db = [
   {
@@ -31,14 +33,23 @@ export default function Home() {
   const characters = db
   const [lastDirection, setLastDirection] = useState<string>()
   let [answer, setAnswer] = useState<string>()
-  
 
-  const swiped = (direction: string, nameToDelete: string) => {
-    
-    
-    if(direction == "left"){
+
+  function onCardAnswered(direction: string, nameToDelete: string) {
+    swiped(direction, nameToDelete)
+    setTimeout(() => {
+      console.log('Waiting');
+    }, 500);
+
+    () => outOfFrame(nameToDelete)
+
+  }
+
+  function swiped(direction: string, nameToDelete: string) {
+
+    if (direction == "left") {
       setAnswer("No")
-    }else{
+    } else {
       setAnswer("Yes")
     }
     /* *let swipeText = new SpeechSynthesisUtterance(
@@ -54,21 +65,27 @@ export default function Home() {
     console.log(name + ' left the screen!')
   }
 
-return (
-  <div>
-  <link href='https://fonts.googleapis.com/css?family=Damion&display=swap' rel='stylesheet' />
-  <link href='https://fonts.googleapis.com/css?family=Alatsi&display=swap' rel='stylesheet' />
-  <h1>Eco Bacchus</h1>
-  <div className='cardContainer'>
-    {characters.map((character) =>
-      <TinderCard className='swipe' preventSwipe={['up', 'down']} key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
-        <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
-          <h3>{character.name}</h3>
-        </div>
-      </TinderCard>
-    )}
-  </div>
-  {answer ? <h2 className='infoText'>You answered {answer}</h2> : <h2 className='infoText' />}
-</div>
-)
+  return (
+    <div>
+      <link href='https://fonts.googleapis.com/css?family=Damion&display=swap' rel='stylesheet' />
+      <link href='https://fonts.googleapis.com/css?family=Alatsi&display=swap' rel='stylesheet' />
+      <h1>Eco Bacchus</h1>
+      <div className='cardContainer'>
+        {characters.map((character) =>
+          <TinderCard className='swipe' preventSwipe={['up', 'down']} key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+            <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
+              <h3>{character.name}</h3>
+            </div>
+            {/* <div className="buttonsRow">
+              <Button type="no" onClickFunction={(dir: string) => swiped(dir = "left", character.name)} />
+              <Button type="yes" onClickFunction={(dir: string) => swiped(dir = "right", character.name)} />
+            </div> */}
+          </TinderCard>
+        )}
+
+
+      </div>
+      {answer ? <h2 className='infoText'>You answered {answer}</h2> : <h2 className='infoText' />}
+    </div>
+  )
 }
