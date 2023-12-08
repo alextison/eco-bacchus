f6f3fed7ef0227e8401b6e3605d8b6f71fd0c0e5
Torfa <alextison@icloud.com>
2023-12-08 02:35:21 +0100

    Create right Card style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,25 +4,11 @@ import TinderCard from 'react-tinder-card'
 
 const db = [
   {
-    name: 'Richard Hendricks',
+    question: 'Will the eath be destroyed by 2050?',
+    leftAnswer: 'No',
+    rightAnswer: 'Yes',
     url: './img/ampoule-dd.jpeg'
   },
-  {
-    name: 'Erlich Bachman',
-    url: './img/ampoule-dd.jpeg'
-  },
-  {
-    name: 'Monica Hall',
-    url: './img/ampoule-dd.jpeg'
-  },
-  {
-    name: 'Jared Dunn',
-    url: './img/ampoule-dd.jpeg'
-  },
-  {
-    name: 'Dinesh Chugtai',
-    url: './img/ampoule-dd.jpeg'
-  }
 ]
 
 export default function Home() {
@@ -46,9 +32,19 @@ return (
   <h1>Eco Bacchus</h1>
   <div className='cardContainer'>
     {characters.map((character) =>
-      <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+      <TinderCard className='swipe' key={character.question} onSwipe={(dir) => swiped(dir, character.question)} onCardLeftScreen={() => outOfFrame(character.question)}>
         <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
-          <h3>{character.name}</h3>
+          <div className='veil'>
+            <div className='answers'>
+              <div className='answer'>
+                <h4>{character.leftAnswer}</h4>
+              </div>
+              <div className='answer'>
+                <h4>{character.rightAnswer}</h4>
+              </div>
+            </div>
+            <h3>{character.question}</h3>
+          </div>
         </div>
       </TinderCard>
     )}
